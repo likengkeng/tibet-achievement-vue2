cb73bf8ed2b38cfd097001c58202449f7d377f6c
hurong <hu.rong@navercorp.com>
2021-06-07 16:57:21 +0800

    update

---
+++ src/h5/views/mock/index.ts
@@ -0,0 +1,119 @@
+export const leaderCareList = [
+  {
+    id: 'test01',
+    content: '习近平回信电视视频习近平回信电视视频',
+    img: '',
+    date: '2014/01/12',
+  },
+  {
+    id: 'test02',
+    content: '中央第六次西藏工作座谈会习近平回信电视视频',
+    img: '',
+    date: '2014/01/12',
+  },
+  {
+    id: 'test03',
+    content: '习总书记今年两会期间接见西藏代表团',
+    img: '',
+    date: '2014/01/12',
+  },
+  {
+    id: 'test04',
+    content: '习总书记今年两会期间接见西藏代表团',
+    img: '',
+    date: '2014/01/12',
+  },
+]
+
+export const eventList = [
+  {
+    id: 'test1',
+    date: '2009年4月1号',
+    content: {
+      img: '/static/imgs/test01.jpg',
+      title: '大标题文案',
+      desc: 'Sass 变量的作用域只能在当前的层级上有效果,如下所示 h1 的样式为它内部定义的 green'
+    },
+  },
+  {
+    id: 'test2',
+    date: '2009年5月1号',
+    content: {
+      img: '/static/imgs/test02.jpg',
+      title: '大标题文案',
+      desc: 'Sass 变量的作用域只能在当前的层级上有效果,如下所示 h1 的样式为它内部定义的 green'
+    },
+  },
+  {
+    id: 'test3',
+    date: '2009年6月1号',
+    content: {
+      img: '/static/imgs/test03.jpg',
+      title: '大标题文案',
+      desc: 'Sass 变量的作用域只能在当前的层级上有效果,如下所示 h1 的样式为它内部定义的 green'
+    },
+  },
+  {
+    id: 'test4',
+    date: '2009年7月1号',
+    content: {
+      img: '/static/imgs/test01.jpg',
+      title: '大标题文案',
+      desc: 'Sass 变量的作用域只能在当前的层级上有效果,如下所示 h1 的样式为它内部定义的 green'
+    },
+  },
+  {
+    id: 'test5',
+    date: '2009年8月1号',
+    content: {
+      img: '/static/imgs/test02.jpg',
+      title: '大标题文案',
+      desc: 'Sass 变量的作用域只能在当前的层级上有效果,如下所示 h1 的样式为它内部定义的 green'
+    },
+  },
+  {
+    id: 'test6',
+    date: '2009年9月1号',
+    content: {
+      img: '/static/imgs/test03.jpg',
+      title: '大标题文案',
+      desc: 'Sass 变量的作用域只能在当前的层级上有效果,如下所示 h1 的样式为它内部定义的 green'
+    },
+  },
+  {
+    id: 'test7',
+    date: '2009年10月1号',
+    content: {
+      img: '/static/imgs/test01.jpg',
+      title: '大标题文案',
+      desc: 'Sass 变量的作用域只能在当前的层级上有效果,如下所示 h1 的样式为它内部定义的 green'
+    },
+  },
+  {
+    id: 'test8',
+    date: '2009年11月1号',
+    content: {
+      img: '/static/imgs/test02.jpg',
+      title: '大标题文案',
+      desc: 'Sass 变量的作用域只能在当前的层级上有效果,如下所示 h1 的样式为它内部定义的 green'
+    },
+  },
+  {
+    id: 'test9',
+    date: '2009年12月1号',
+    content: {
+      img: '/static/imgs/test03.jpg',
+      title: '大标题文案',
+      desc: 'Sass 变量的作用域只能在当前的层级上有效果,如下所示 h1 的样式为它内部定义的 green'
+    },
+  },
+  {
+    id: 'test10',
+    date: '2010年4月1号',
+    content: {
+      img: '/static/imgs/test01.jpg',
+      title: '大标题文案',
+      desc: 'Sass 变量的作用域只能在当前的层级上有效果,如下所示 h1 的样式为它内部定义的 green'
+    },
+  }
+];
